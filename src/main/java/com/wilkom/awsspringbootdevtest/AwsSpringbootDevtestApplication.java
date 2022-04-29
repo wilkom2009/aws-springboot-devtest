f54f9242c439ed0aff1e733aa7fbacb0480d7663
Wilson KOMLAN <koffikomlan01@gmail.com>
2022-04-29 22:07:11 +0000

    Project boostraping with /api/hello resource

---
+++ src/main/java/com/wilkom/awsspringbootdevtest/AwsSpringbootDevtestApplication.java
@@ -0,0 +1,13 @@
+package com.wilkom.awsspringbootdevtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringbootDevtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringbootDevtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilkom/awsspringbootdevtest/AwsSpringbootDevtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilkom.awsspringbootdevtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringbootDevtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
